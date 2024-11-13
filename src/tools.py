e906cd8752f57544e3b65fea04a4a6cf3c3d7afe
Kam <52869955+kamyarscode@users.noreply.github.com>
2024-11-13 11:34:22 -0700

    add summarize feature and error handling

--- src/tools.py
+++ src/tools.py
@@ -1,4 +1,38 @@
-from server.ollama import generate
+import server.ollama as ollama
+from src.load_config import project_config
 
-def summarize_tool():
-    pass
+import json
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+    handlers=[
+            logging.StreamHandler() # log to console from here.
+        ] 
+)
+
+default_model = project_config['models']['llama3.1']
+
+def summarize_tool(input_txt: str, default_model=default_model) -> dict:
+    model = default_model
+
+    SYSTEM_PROMPT = "Summarize this text in a few sentences."
+
+    prompt = f"context: ```{input_txt}``` \n\n output: "
+    response, _ = ollama.generate(model_name=model, system=SYSTEM_PROMPT, prompt=prompt)
+    try:
+        result_output = {
+            "original_text": input_txt,
+            "model_used": model,
+            "summary_text": response
+        }
+
+    except json.decoder.JSONDecodeError as e:
+        logging.info(f"\n\nOutput not valid for response: {response}")
+        logging.info(f"\n\nException thrown - {e}")
+        result_output = None
+    
+    return result_output
+
+    
