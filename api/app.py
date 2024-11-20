0659c919a5980b8b2f856af0a42228309cdbeb20
Kam <52869955+kamyarscode@users.noreply.github.com>
2024-11-19 17:08:35 -0700

    add some api endpoints

--- api/app.py
+++ api/app.py
@@ -7,7 +7,36 @@ app = Flask(__name__)
 
 # Test endpoint here.
 
-@app.route('/apt/test')
+@app.route('/api/test')
 def test():
     
     return "This is test endpoint."
+
+@app.route('/api/version')
+def get_version():
+
+    data = {
+        "status": 200,
+        "version": "0.1"
+    }
+
+    return data
+
+@app.route('/')
+def homepage():
+        
+    data = {
+        "status": 200,
+        "message": "App is running."
+    }
+
+# Return server client version
+@app.route('/api/cv')
+def get_client_version():
+
+    response, status, version = return_client_version()
+    data = {
+        "status": status,
+        "message": response,
+        "llm_server_version": version
+    }
