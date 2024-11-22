e1b076c98fe87b3e708e53d1ff56f350c92df8ed
Kam <52869955+kamyarscode@users.noreply.github.com>
2024-11-22 12:37:30 -0700

    add model utilization function

--- src/use_model.py
+++ src/use_model.py
@@ -1,16 +1,41 @@
+from server.ollama import client
+import json
+
 def prompt_model(input_str: str, metadata={}, model="default"):
 
     SYSTEM_PROMPT_DICT = {
         "prompt_to_use": check_which_prompt(model)
     }
 
-# Set model hyperparameters. Do this with config file later.
-model_config = {
-    'num_ctx': 8096,
-    'num_predict': 2048,
-    'top_k': 30,
-    'top_p': 0.85,
-    'repeat_last_n': 512,
-    'temperature': 0.5
-}
+    # Set model hyperparameters. Do this with config file later.
+    model_config = {
+        'num_ctx': 8096,
+        'num_predict': 2048,
+        'top_k': 30,
+        'top_p': 0.85,
+        'repeat_last_n': 512,
+        'temperature': 0.5
+    }
+
+    SYSTEM_PROMPT = SYSTEM_PROMPT_DICT['prompt_to_use'][1]
+
+    # User prompt begins here
+    user_prompt = f"{input_str}<|eot_id|>\n<|start_header_id|>assistant<|end_header_id|>"
+    response, _ = client.generate(model_name=model,system=SYSTEM_PROMPT,prompt=user_prompt,options=model_config)
+
+    try:
+
+        result=json.loads(response, strict=False)
+
+        # Check if result is a list and not empty.
+        if isinstance(result,list) and len(result) > 0:
+            result = [dict(item, **metadata) for item in result]
+        else:
+            result = None
+
+    except Exception as e:
+        print ("Error with output, view response {response}, {e}")
+        result = None
+
+    return result
 
