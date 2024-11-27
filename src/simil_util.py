b69ab6b17a704b29955b914c58fa5c1ade135c48
Kam <52869955+kamyarscode@users.noreply.github.com>
2024-11-27 10:31:15 -0700

    add simil util function

---
+++ src/simil_util.py
@@ -0,0 +1,18 @@
+import json
+import numpy as np
+import faiss
+import logging
+
+from sentence_transformers import SentenceTransformer
+from sklearn.metrics.pairwise import cosine_similarity
+
+# Initialize model
+model = SentenceTransformer('paraphrase-MiniLM-L6-v2') # investigate this model and get performance metrics
+
+# Start of function to get similarities using embeddings between phrases found in text
+def find_similar_phrases(embedding_dimension, phrases, query_embedding, top_k=5):
+    index = faiss.IndexFlatL2(embedding_dimension)
+    phrase_embedding = model.encode(phrases, show_progress_bar=True)
+
+    D, I = index.search(np.array([query_embedding]), top_k)
+    return [(phrases[idx], D[0][i]) for i, idx in enumerate(I[0])]
