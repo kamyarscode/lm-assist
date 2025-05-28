f121562e6903522897e5cd4cf0ccc066fcc53f46
kamyar <52869955+kamyarscode@users.noreply.github.com>
2025-05-27 22:43:19 -0600

    add really rough conversation ability and short term memory to model

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import json
 import time
-
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify, session
 from server.ollama import ollama_health
 from src.use_model import prompt_model
+from api.chat_utils import get_chat_history
+from src.tools import chat_tool
 app = Flask(__name__)
 
 
@@ -15,14 +16,33 @@ def process():
     result = text1 + text2
     return render_template('main_page.html', result=result)
 
+@app.route('/chat', methods=['POST'])
+def chat_func():
+    # Get user input from the request
+    user_input = request.json.get("message")
+
+
+    if not user_input:
+        return jsonify({"error": "Empty input"}), 400
+
+
+    chat_tool_response = chat_tool(chat_history=get_chat_history(), input_txt=user_input)
+
+    session['chat_history'] = chat_tool_response['chat_history']
+    print ("Current chat history: ", session['chat_history'])
+
+    # Set up session to store chat history.
+
+    return jsonify({
+        "response": chat_tool_response["model_response"],})
 
 # Main page that gets served here. Used for testing endpoints.
 @app.route('/')
 def index():
     """Serve the web page."""
-    return render_template("main_page.html")
-# Test endpoint here.
+    return render_template("chat.html")
 
+# Test endpoint here.
 @app.route('/api/test')
 def test():
     input_str = "The National Aeronautics and Space Administration (NASA /ˈnæsə/) is an independent agency of the US federal government responsible for the United States' civil space program, aeronautics research and space research. Established in 1958, it succeeded the National Advisory Committee for Aeronautics (NACA) to give the US space development effort a distinct civilian orientation, emphasizing peaceful applications in space science. It has since led most of America's space exploration programs, including Project Mercury, Project Gemini, the 1968–1972 Apollo Moon landing missions, the Skylab space station, and the Space Shuttle. Currently, NASA supports the International Space Station (ISS) along with the Commercial Crew Program, and oversees the development of the Orion spacecraft and the Space Launch System for the lunar Artemis program. NASA's science division is focused on better understanding Earth through the Earth Observing System; advancing heliophysics through the efforts of the Science Mission Directorate's Heliophysics Research Program; exploring bodies throughout the Solar System with advanced robotic spacecraft such as New Horizons and planetary rovers such as Perseverance; and researching astrophysics topics, such as the Big Bang, through the James Webb Space Telescope, the four Great Observatories, and associated programs. The Launch Services Program oversees launch operations for its uncrewed launches."
@@ -54,6 +74,9 @@ def get_client_version():
     return data
 
 def main():
+    # Set defaults. Change this later. 
+    app.secret_key = "super secret key"
+
     app.run(port=7777, debug=False)
     print ("Server is running now.")
 
--- src/tools.py
+++ src/tools.py
@@ -1,6 +1,6 @@
 import server.ollama as ollama_client
 from src.load_config import project_config
-
+from api.chat_utils import add_to_chat_history
 import json
 import logging
 
@@ -13,7 +13,7 @@ logging.basicConfig(
     )
 
 
-default_model = project_config['models']['llama3.1']
+default_model = project_config['models']['default']
 llama3_instruct = project_config['models']['llama3.2-instruct']
 
 def summarize_tool(input_txt: str, default_model=default_model) -> dict:
@@ -28,6 +28,37 @@ def summarize_tool(input_txt: str, default_model=default_model) -> dict:
             "original_text": input_txt,
             "model_used": model,
             "summary_text": response
+        }
+
+    except json.decoder.JSONDecodeError as e:
+        logging.info(f"\n\nOutput not valid for response: {response}")
+        logging.info(f"\n\nException thrown - {e}")
+        result_output = None
+    
+    return result_output
+
+# 
+def chat_tool(chat_history, input_txt: str, default_model=default_model) -> dict:
+    model = default_model
+
+    SYSTEM_PROMPT = "Your role is to chat with the user. Recall previous messages in the chat history to provide relevant responses. " \
+                    "If you don't know the answer, say 'I don't know'."
+
+    prompt = f"chat history context: ```{list(chat_history)}``` user: ```{input_txt}``` \n\n output: "
+
+    response, _ = ollama_client.generate(model_name=model, system=SYSTEM_PROMPT, prompt=prompt)
+    print ("Adding this INPUT: ", input_txt)
+    print ("Adding this RESPONSE: ", response)
+
+    chat_history = add_to_chat_history(chat_history, user_input=input_txt, llm_response=response)
+
+    print ("New chat history: ", chat_history)
+    try:
+        result_output = {
+            "original_text": input_txt,
+            "model_used": model,
+            "chat_history": list(chat_history),
+            "model_response": response
         }
 
     except json.decoder.JSONDecodeError as e:
